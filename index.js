6587902e36a1cdcf1e5979883c61260b5cc3d587
EttoNekko <100702053+EttoNekko@users.noreply.github.com>
2023-12-28 10:52:33 +0700

    chỉnh lại port để thử host

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ mongoose.connect(process.env.MONGODB)
 
 app.use('/api', routes)
 
-app.listen(8000, () => {
+var port = normalizePort(process.env.PORT || '8000');
+
+app.listen(port, () => {
   console.log('run server...')
 })
+
+/* Normalize a port into a number, string, or false.*/
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
